cb99292b41750f75abd59381dbea2f11ff11ce0f
unknown <varunkumar262009@gmail.com>
2025-05-14 17:45:34 +0530

    source files and components of little-lemon

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navigation from './components/navigation/nav'
+import Home from './components/home/home'
+import About from './components/about/about'
+import Menu from './components/menu/menu'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Home />
+      <About />
+      <Menu />
+    </>
+  )
+}
+
+export default App
